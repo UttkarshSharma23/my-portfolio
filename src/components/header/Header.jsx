a67fec9a0b22a43b362c421373fdeaeaf6428bfa
UttkarshSharma23 <uttkarsh.sharma2019@gmail.com>
2024-02-25 03:02:26 +0530

    profile pic

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './header.css'
 import CTA from './CTA'
-import ME from '../../assets/uttkarshs.png'
+import ME from '../../assets/us.png'
 import HeaderScoials from './HeaderScoials'
 const Header = () => {
   return (
